94eccf90db4f78542180e779c46a84ed9d992917
m7mdev1 <69204096+M7MDdev1@users.noreply.github.com>
2025-09-06 18:44:23 +0300

    rename: welcome label to AbuCode

--- src/vs/workbench/contrib/welcomeGettingStarted/common/gettingStartedContent.ts
+++ src/vs/workbench/contrib/welcomeGettingStarted/common/gettingStartedContent.ts
@@ -247,7 +247,9 @@ function createCopilotSetupStep(id: string, button: string, when: string, includ
 export const walkthroughs: GettingStartedWalkthroughContent = [
 	{
 		id: 'Setup',
-		title: localize('gettingStarted.setup.title', "Get started with VS Code"),
+		// title: localize('gettingStarted.setup.title', "Get started with VS Code"),
+		title: "Get Started with AbuCode",
+
 		description: localize('gettingStarted.setup.description', "Customize your editor, learn the basics, and start coding"),
 		isFeatured: true,
 		icon: setupIcon,
