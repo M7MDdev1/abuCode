ddb5d01bbea21b1d075f6224495b835ca0a24a67
m7mdev1 <69204096+M7MDdev1@users.noreply.github.com>
2025-09-19 17:01:48 +0300

    simple auxbar

--- src/vs/workbench/contrib/blankView/browser/blankViewPane.ts
+++ src/vs/workbench/contrib/blankView/browser/blankViewPane.ts
@@ -6,54 +6,12 @@
 import { localize } from '../../../../nls.js';
 import * as DOM from '../../../../base/browser/dom.js';
 
-import { ViewPane, IViewPaneOptions } from '../../../browser/parts/views/viewPane.js';
+import { ViewPane } from '../../../browser/parts/views/viewPane.js';
 import { IActionViewItemOptions } from '../../../../base/browser/ui/actionbar/actionViewItems.js';
 import { DisposableStore } from '../../../../base/common/lifecycle.js';
 
-// خدمات لازمة لِـ ViewPane (حقن تبعي)
-import { IKeybindingService } from '../../../../platform/keybinding/common/keybinding.js';
-import { IContextMenuService } from '../../../../platform/contextview/browser/contextView.js';
-import { IConfigurationService } from '../../../../platform/configuration/common/configuration.js';
-import { IContextKeyService } from '../../../../platform/contextkey/common/contextkey.js';
-import { IViewDescriptorService } from '../../../common/views.js';
-import { IInstantiationService } from '../../../../platform/instantiation/common/instantiation.js';
-import { IOpenerService } from '../../../../platform/opener/common/opener.js';
-import { IThemeService } from '../../../../platform/theme/common/themeService.js';
-import { IHoverService } from '../../../../platform/hover/browser/hover.js';
-// هذا اختياري (بعض الفروع قد تختلف مساره):
-import { IAccessibleViewInformationService } from '../../../services/accessibility/common/accessibleViewInformationService.js';
 export class BlankViewPane extends ViewPane {
-	constructor(
-		options: IViewPaneOptions,
-		@IKeybindingService keybindingService: IKeybindingService,
-		@IContextMenuService contextMenuService: IContextMenuService,
-		@IConfigurationService configurationService: IConfigurationService,
-		@IContextKeyService contextKeyService: IContextKeyService,
-		@IViewDescriptorService viewDescriptorService: IViewDescriptorService,
-		@IInstantiationService instantiationService: IInstantiationService,
-		@IOpenerService openerService: IOpenerService,
-		@IThemeService themeService: IThemeService,
-		@IHoverService hoverService: IHoverService,
-		@IAccessibleViewInformationService accessibleViewInformationService?: IAccessibleViewInformationService
-	) {
-		super(
-			{
-				...options,
-				titleMenuId: undefined,
-				expanded: true,
-			},
-			keybindingService,
-			contextMenuService,
-			configurationService,
-			contextKeyService,
-			viewDescriptorService,
-			instantiationService,
-			openerService,
-			themeService,
-			hoverService,
-			accessibleViewInformationService
-		);
-	}
+
 	protected override renderBody(container: HTMLElement): void {
 		super.renderBody(container);
 		const _register = this._register.bind(this);
